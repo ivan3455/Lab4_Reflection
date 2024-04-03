e35b42b5a79935848c768ce2ca94331f96b83841
ivan3455 <trunovivan641@gmail.com>
2024-04-03 21:25:57 +0300

    Add project files.

---
+++ ProductFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ProductFactory
+{
+    // Поля класу
+    private string factoryName;
+    public int employeesCount;
+    protected double totalRevenue;
+    internal bool isOperating;
+    public DateTime lastMaintenanceDate;
+
+    // Конструктор класу
+    public ProductFactory(string name, int employees, DateTime maintenanceDate)
+    {
+        factoryName = name;
+        employeesCount = employees;
+        lastMaintenanceDate = maintenanceDate;
+        isOperating = true;
+        totalRevenue = 0;
+    }
+
+    // Метод для додавання до загального доходу
+    public void AddRevenue(double revenue)
+    {
+        totalRevenue += revenue;
+    }
+
+    // Метод для зміни назви заводу
+    public void ChangeFactoryName(string newName)
+    {
+        factoryName = newName;
+    }
+
+    // Метод для проведення технічного обслуговування
+    protected void PerformMaintenance()
+    {
+        Console.WriteLine("Performing maintenance on the factory...");
+        // Код для обслуговування
+        lastMaintenanceDate = DateTime.Now;
+    }
+
+    // Метод для відображення інформації про завод
+    public void DisplayFactoryInfo()
+    {
+        Console.WriteLine($"Factory Name: {factoryName}");
+        Console.WriteLine($"Employees Count: {employeesCount}");
+        Console.WriteLine($"Total Revenue: {totalRevenue}");
+        Console.WriteLine($"Last Maintenance Date: {lastMaintenanceDate}");
+        Console.WriteLine($"Is Operating: {(isOperating ? "Yes" : "No")}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Отримання тип класу ProductFactory
+        Type factoryType = typeof(ProductFactory);
+
+        // Використання TypeInfo
+        TypeInfo factoryTypeInfo = factoryType.GetTypeInfo();
+
+        // Отримання інформацію про всі члени класу (поля, методи, властивості, тощо)
+        MemberInfo[] members = factoryType.GetMembers();
+
+        Console.WriteLine("Members of ProductFactory class:");
+        foreach (var member in members)
+        {
+            Console.WriteLine(member.Name);
+        }
+
+        Console.WriteLine();
+
+        // Отримуємо інформацію про всі поля класу
+        FieldInfo[] fields = factoryType.GetFields();
+
+        Console.WriteLine("Fields of ProductFactory class:");
+        foreach (var field in fields)
+        {
+            Console.WriteLine(field.Name);
+        }
+
+        Console.WriteLine();
+
+        // Отримання інформацію про всі методи класу
+        MethodInfo[] methods = factoryType.GetMethods();
+
+        Console.WriteLine("Methods of ProductFactory class:");
+        foreach (var method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
+
+        Console.WriteLine();
+
+        // Виклик методу AddRevenue через Reflection
+        ProductFactory factory = new ProductFactory("MyFactory", 100, DateTime.Now);
+        MethodInfo addRevenueMethod = factoryType.GetMethod("AddRevenue");
+        object[] parameters = new object[] { 10000 }; // Параметри методу
+        addRevenueMethod.Invoke(factory, parameters); // Виклик методу
+
+        // Відображення оновленої інформації про завод
+        factory.DisplayFactoryInfo();
+    }
+}
